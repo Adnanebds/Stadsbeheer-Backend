d06c905cb599676818c3b9f07220bba2422a74f3
Adnanebds <adnanebadysoussi47@gmail.com>
2025-04-17 15:22:01 +0200

    supabase postgressql

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "postgresql://postgres:1&5819tGw/8^@db.yqlpqgsoynbtvhwprjyt.supabase.co:5432/postgres"  # Replace with your Supabase details
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
 @app.get("/omar")
 def read_root():
@@ -22,3 +34,7 @@ def read_root():
 @app.get("/adnane")
 def read_root():
     return {"Hoi": "adanane"}
+
+@app.get("/")
+def read_root(db=Depends(get_db)):
+    return {"message": "Connected to Supabase DB!"}
